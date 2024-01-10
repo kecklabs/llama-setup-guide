91b46790ee34038acd2d5358376bc603ccd18899
asrouji <72050533+asrouji@users.noreply.github.com>
2024-01-09 16:57:06 -0800

    remove extra comma in comment

--- sample_query.py
+++ sample_query.py
@@ -3,7 +3,7 @@ from llama_cpp import Llama
 llm = Llama(
     model_path="orca_mini_v3_7b.Q4_K_M.gguf",  # Path to model file
     n_ctx=4096,  # Context window size
-    n_gpu_layers=-1,  # -1 to use all GPU layers, 0 to use only CPU,
+    n_gpu_layers=-1,  # -1 to use all GPU layers, 0 to use only CPU
     verbose=False  # Whether to print debug info
 )
 
